b936b01c38bd78f3cac712f068b48a8dd336d4b7
alsun <aminabdo8@gmail.com>
2017-04-01 02:25:20 +0200

    starting using ajax to send model

--- includes/footer.php
+++ includes/footer.php
@@ -2,16 +2,32 @@
     &copy; Copyright 2015-2016 Shaunta's Boutique
 </footer>
 
-<?php
-include 'detailmodal.php';
-?>
+
 
 <!--js libraries-->
 <script src="js/jquery-3.1.1.min.js"></script>
 
 <script src="js/bootstrap.min.js"></script>
 <script src="js/script.js"></script>
-
+<script>
+    function detail(id) {
+        var data ={"id":id};
+        $.ajax({
+            url:'/ecommerce/includes/detailmodal.php',
+            method:'post',
+            data:data,
+            success:function(data){
+                $('body').append(data);
+                $('#detail-modal').modal('toggle');
+            },
+            error:
+            function () {
+                alert('something is wrong,please call customer service');
+            }
+            
+        });
+    }
+</script>
 
 
 </body>
